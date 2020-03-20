f1a02cca5be11cbff5c0a93b924b971906de3d37
Nur Fikri <codingki@gmail.com>
2020-03-20 11:53:09 +0700

    taxiapp

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Kiki";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 2790;
+
+            taxi.TaxiInfo();
+            Console.WriteLine("------------------"); 
+            taxi.PickUpPassanger();
+            Console.WriteLine("------------------");
+            taxi.DropOffPassanger();
+            Console.ReadKey();
+        }
+    }
+
+    public class Taxi{
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+        Console.WriteLine("Nama Driver : {0}", DriverName);
+        Console.WriteLine("Sedang Bekerja : {0}", OnDuty);
+        Console.WriteLine("Nomor Penumpang : {0}", NumPassanger);
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("Sedang Mengangkut penumpang");
+        
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("Menurunkan penumpang");
+
+        }
+    }
+}
